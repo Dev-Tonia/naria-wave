f4a7b67a17dcc77788ddad3b473f4e81eeebc595
Dev-Tonia <ogunwaanthonia@gmail.com>
2024-01-05 01:52:17 +0100

    added the navbar active state

--- src/components/Layout/NavbarItem.jsx
+++ src/components/Layout/NavbarItem.jsx
@@ -5,7 +5,7 @@ import "../Layout/layout.css";
 
 export default function NavbarItem({ title, icon, path }) {
   return (
-    <div className=" text-center px-3 border-bottom border-2 border-warning py-2  navbar-item">
+    <div className=" text-center px-3  py-2  navbar-item">
       <NavLink to={path} className="text-body " activeClassName="activeLink">
         <span>{icon}</span>
         <p className=" mb-0 fs-6 text-warning fw-medium title">{title} </p>
